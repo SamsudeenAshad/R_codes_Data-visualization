64c77eedfcc690902fe999720393be59e8a6db59
Sim Shad <samsudeenashad@icloud.com>
2025-01-08 18:45:52 +0530

    Update data_visulaixation first day.R

--- data_visulaixation first day.R
+++ data_visulaixation first day.R
@@ -15,8 +15,15 @@ library(ggplot2)
 #Filter for the "dino" dataset
 dino_data <- subset(datasaurus_dozen,dataset == "dino")
 
+#create a scatter plot
 ggplot(dino_data, aes(x = x, y = y)) +
   geom_point(color = 'blue', size = 2) +
   theme_minimal() +
   labs(title = "Datasaurus: Dino", x = "X", y = "Y")
 
+#create a scatter plot for all datasets
+ggplot(datasaurus_dozen, aes(x = x, y = y)) +
+  geom_point(alpha = 0.7) +
+  facet_wrap(~ dataset, scales = "free") +
+  theme_minimal() +
+  labs(title = "Datasaurus Dozen", x = "X", y = "Y")
